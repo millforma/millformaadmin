a12d893df24d60cbee141dab511e1b0e76a2a54c
aymenb@mill-forma.fr <Aymen1999!>
2022-10-24 14:29:39 +0200

    modified csp rules 6

--- electronicSignature/settings.py
+++ electronicSignature/settings.py
@@ -170,10 +170,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
 ]
 CSP_DEFAULT_SRC = ("'none'",)
-CSP_STYLE_SRC = ("'self'", 'unsafe-inline', 'fonts.googleapis.com', 'cdn.datatables.net', 'stackpath.bootstrapcdn.com')
-CSP_SCRIPT_SRC = ("'self'",)
-CSP_FONT_SRC = ("'self'", 'fonts.gstatic.com', 'fonts.googleapis.com',)
-CSP_IMG_SRC = ("'self'",)
-CSP_CONNECT_SRC = ("'self'", 'millforma-admin.fr',)
-CSP_FRAME_SRC = ("'self'", 'documentcloud.adobe.com', "'unsafe-inline'",)
+CSP_STYLE_SRC = ("'self'", 'fonts.googleapis.com', 'cdn.datatables.net', 'stackpath.bootstrapcdn.com',"'unsafe-inline'",)
+CSP_SCRIPT_SRC = ("'self'","'unsafe-inline'","'unsafe-eval'",)
+CSP_FONT_SRC = ("'self'", 'fonts.gstatic.com', 'fonts.googleapis.com',"'unsafe-inline'",)
+CSP_IMG_SRC = ("'self'","'unsafe-inline'",)
+CSP_CONNECT_SRC = ("'self'", 'millforma-admin.fr',"'unsafe-inline'","'unsafe-eval'",)
+CSP_FRAME_SRC = ("'self'", 'documentcloud.adobe.com', "'unsafe-inline'","'unsafe-eval'",)
 CSP_INCLUDE_NONCE_IN = ("script-src",)
